b9f44d652e0e729ac0f718aaec403433a09d346e
Haim <haimtzabar@gmail.com>
2013-07-07 16:15:12 +0300

    Signed-off-by: Haim <haimtzabar@gmail.com>

--- src/il/co/moshavit/model/User.java
+++ src/il/co/moshavit/model/User.java
@@ -10,5 +10,49 @@ public class User {
   private String Phone;
   private String Password;
   private DateTime RegisterDate;
+  
+  
+	public int getIdUser() {
+		return IdUser;
+	}
+	public void setIdUser(int idUser) {
+		IdUser = idUser;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getFamily() {
+		return Family;
+	}
+	public void setFamily(String family) {
+		Family = family;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public DateTime getRegisterDate() {
+		return RegisterDate;
+	}
+	public void setRegisterDate(DateTime registerDate) {
+		RegisterDate = registerDate;
+	}  
 
 }
