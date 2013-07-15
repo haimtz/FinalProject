a093cb0a1d08876a5fc508aa3449e64c51490269
haimtz <haimtzabar@gmail.com>
2013-07-15 20:33:35 +0300

    complete database class

--- src/il/co/moshavit/service/DataBase.java
+++ src/il/co/moshavit/service/DataBase.java
@@ -2,13 +2,20 @@ package il.co.moshavit.service;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.joda.time.DateTime;
 import org.xml.sax.SAXException;
+
+import com.mysql.jdbc.ResultSet;
 
 public class DataBase {
 	
@@ -18,28 +25,56 @@ public class DataBase {
 	private final String USERNAME;
 	private final String PASSWORD;
 	
+	private Statement statement = null;
+	private PreparedStatement preper = null;
+	
+	
 	public DataBase() throws Exception
 	{
+		// database settings
 		JDBC_DRIVER = Settings.getData("driver");
 		DATABASE_URL = Settings.getData("url");
 		USERNAME =  Settings.getData("username");
-		PASSWORD = Settings.getData("password");
+		PASSWORD = Settings.getData("password");		
 		
+		// Register JDBC driver
 		Class.forName(JDBC_DRIVER);
+				
+		// Open connection
 		connection = DriverManager.getConnection(DATABASE_URL,USERNAME,PASSWORD);
 	}
 	
-	public void create(String sql, String... values) throws Exception
+	public void query(String sql) throws SQLException
 	{
-		PreparedStatement statement = connection.prepareStatement(sql);
-		
-		for(int index = 0 ; index < values.length ; index++)
-		{
-			statement.setString(index+1, values[index]);
-		}
-		
-		statement.execute();
+		preper = connection.prepareStatement(sql);
 	}
 	
+	public void addParamString(int index, String value) throws SQLException
+	{
+		preper.setString(index, value);
+	}
+	
+	public void addParamInt(int index, int value) throws SQLException
+	{
+		preper.setInt(index, value);
+	}
+	
+	public void addParamDate(int index, DateTime value) throws SQLException
+	{
+		preper.setDate(index, new Date(value.toDate().getTime()));
+	}
+	
+	public void create() throws SQLException
+	{
+		preper.executeUpdate();
+	}
+	
+	public ResultSet resultQuery() throws SQLException
+	{
+		ResultSet result = null;		
+		result = (ResultSet) preper.executeQuery();
+		
+		return result;
+	}
 
 }
